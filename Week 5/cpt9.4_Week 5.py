b28287ce9b2d9b89bdaa0385f8b6f32d5d14d9b6
Angela Song <71359749+Angelawonderfulworld@users.noreply.github.com>
2020-11-28 18:17:46 -0500

    Add files via upload

---
+++ Week 5/cpt9.4_Week 5.py
@@ -0,0 +1,50 @@
+#9.4 Write a program to read through the mbox-short.txt and figure out who has sent the greatest number of mail messages. The program looks for 'From ' lines and takes the second word of those lines as the person who sent the mail. The program creates a Python dictionary that maps the sender's mail address to a count of the number of times they appear in the file. After the dictionary is produced, the program reads through the dictionary using a maximum loop to find the most prolific committer.
+
+#code 1
+name = input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name)
+words = list()
+count = dict()
+
+for line in handle:
+    line = line.rstrip()
+    if line.startswith('From ') :
+        line = line.split()
+        words.append(line[1])
+
+for w in words:
+    count[w]= count.get(w,0)+1
+
+bigcount =None
+bigword = None
+for key,values in count.items():
+    if bigcount is None or values>bigcount:
+        bigcount = values
+        bigword = key
+print(bigword,bigcount)
+
+#code 2
+name = input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+text = open(name)
+
+maxauthor = dict()
+
+for line in text:
+    line.rstrip()
+    if not line.startswith("From "): continue
+    words = line.split()
+    maxauthor[words[1]] = maxauthor.get(words[1],0)+1
+
+largest = None
+largest_author = None
+
+for key in maxauthor:
+    if largest is None: largest = maxauthor[key]
+
+    if largest < maxauthor[key]:
+        largest = maxauthor[key]
+        largest_author = key
+
+print(largest_author, largest)
