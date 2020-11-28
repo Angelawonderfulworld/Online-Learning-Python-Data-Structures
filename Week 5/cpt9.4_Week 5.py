c87ed5bbbca0713172e884e90fc82ee05b8df8f6
Angela Song <71359749+Angelawonderfulworld@users.noreply.github.com>
2020-11-28 18:21:06 -0500

    Update cpt9.4_Week 5.py

--- Week 5/cpt9.4_Week 5.py
+++ Week 5/cpt9.4_Week 5.py
@@ -1,6 +1,6 @@
 #9.4 Write a program to read through the mbox-short.txt and figure out who has sent the greatest number of mail messages. The program looks for 'From ' lines and takes the second word of those lines as the person who sent the mail. The program creates a Python dictionary that maps the sender's mail address to a count of the number of times they appear in the file. After the dictionary is produced, the program reads through the dictionary using a maximum loop to find the most prolific committer.
 
-#code 1
+#Solution 1
 name = input("Enter file:")
 if len(name) < 1 : name = "mbox-short.txt"
 handle = open(name)
@@ -24,7 +24,9 @@ for key,values in count.items():
         bigword = key
 print(bigword,bigcount)
 
-#code 2
+
+
+#Solution 2
 name = input("Enter file:")
 if len(name) < 1 : name = "mbox-short.txt"
 text = open(name)
